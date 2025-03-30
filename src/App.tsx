4a6ff5cdfd9aae9a0a1fde2c4f7682722eb0a69b
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-03-30 16:49:45 +0300

    task sokution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,63 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { getTodos } from './api';
+import { Todo } from './types/Todo';
+import { TodoStatus } from './TodoStatus';
 
 export const App: React.FC = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [isTodosLoading, setIsTodosLoading] = useState(false);
+  const [filterByStatus, setFilterByStatus] = useState<TodoStatus>(
+    TodoStatus.ALL,
+  );
+  const [searchByName, setSearchByName] = useState<string>('');
+  const [selectedTodo, setSelectedTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setIsTodosLoading(true);
+    getTodos().then(todos => {
+      // eslint-disable-next-line no-console
+      console.log(todos);
+      setTodoList(todos);
+      setIsTodosLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    setIsTodosLoading(true);
+    getTodos()
+      .then(todos => {
+        if (filterByStatus === TodoStatus.ALL) {
+          return todos;
+        }
+
+        return todos.filter(todo => {
+          switch (filterByStatus) {
+            case TodoStatus.ACTIVE:
+              return todo.completed === false;
+            case TodoStatus.COMPLETED:
+              return todo.completed === true;
+          }
+        });
+      })
+      .then(todos => {
+        if (searchByName.length === 0) {
+          return todos;
+        }
+
+        return todos.filter(todo =>
+          todo.title.toLocaleLowerCase().includes(searchByName.toLowerCase()),
+        );
+      })
+      .then(todos => {
+        // eslint-disable-next-line no-console
+        console.log(todos);
+        setTodoList(todos);
+        setIsTodosLoading(false);
+      });
+  }, [filterByStatus, searchByName]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +72,38 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                selectedStatus={filterByStatus}
+                onSelectStatus={newStatus => setFilterByStatus(newStatus)}
+                searchValue={searchByName}
+                onSearchValueChange={searchValue =>
+                  setSearchByName(searchValue)
+                }
+                onSearchValueClear={() => setSearchByName('')}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isTodosLoading ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  todoList={todoList}
+                  selectedTodo={selectedTodo}
+                  setSelectedTodo={setSelectedTodo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {selectedTodo && (
+        <TodoModal
+          selectedTodo={selectedTodo}
+          onClose={() => setSelectedTodo(null)}
+        />
+      )}
     </>
   );
 };
